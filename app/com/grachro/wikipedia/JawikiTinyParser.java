77d74539b56ac3283e3c1600b6b3f815aa5c1732
grachro <grachro@gmail.com>
2014-05-25 00:45:44 +0900

    add JawikiTinyParser and jawikiDbInsert

---
+++ app/com/grachro/wikipedia/JawikiTinyParser.java
@@ -0,0 +1,112 @@
+package com.grachro.wikipedia;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.LineIterator;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * 概要
+ * Wikipediaで配布されているコンテンツxmlから、タイトルとカテゴリを抜出す超簡易パーサです
+ * 
+ * 使い方
+ * wget http://dumps.wikimedia.org/jawiki/20140503/jawiki-20140503-pages-meta-current.xml.bz2
+ * bunzip2 jawiki-20140503-pages-meta-current.xml.bz2
+ * JawikiTinyParserを実行
+ *
+ */
+public class JawikiTinyParser extends DefaultHandler {
+
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+
+		String filePath = args[0];
+
+		SAXParserFactory spfactory = SAXParserFactory.newInstance();
+		SAXParser parser = spfactory.newSAXParser();
+
+		parser.parse(new File(filePath), new JawikiTinyParser());
+	}
+
+	boolean titleTag = false;
+	boolean nsTag = false;
+	boolean idTag = false;
+	boolean textTag = false;
+
+	private String currentTitle = "";
+	private String currentNameSpace = "-1";
+	private String currentId = "-1";
+	private StringBuilder currentPageContents = new StringBuilder();
+
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) {
+		clearTagFlags();
+		if ("title".equals(qName)) {
+			this.titleTag = true;
+		} else if ("ns".equals(qName)) {
+			this.nsTag = true;
+		} else if ("id".equals(qName)) {
+			idTag = true;
+		} else if ("text".equals(qName)) {
+			this.currentPageContents = new StringBuilder();
+			textTag = true;
+		}
+	}
+
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		if (this.textTag) {
+			String output = "id=" + this.currentId + ",namespace=" + this.currentNameSpace + ",title=" + this.currentTitle;
+			System.out.println(output);
+
+			String contents = this.currentPageContents.toString();
+			LineIterator itr = IOUtils.lineIterator(new StringReader(contents));
+			while (itr.hasNext()) {
+				String line = itr.next();
+				if (line.startsWith("[[Category")) {
+					System.out.println("¥t" + line);
+				}
+			}
+		}
+		clearTagFlags();
+	}
+
+	private void clearTagFlags() {
+		this.titleTag = false;
+		this.nsTag = false;
+		this.idTag = false;
+		this.textTag = false;
+	}
+
+	@Override
+	public void characters(char[] ch, int offset, int length) throws SAXException {
+		String text = new String(ch, offset, length);
+		try {
+			parse(text);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void parse(String text) throws IOException {
+		if (this.titleTag) {
+			this.currentTitle = text;
+		} else if (this.nsTag) {
+			this.currentNameSpace = text;
+		} else if (this.idTag) {
+			this.currentId = text;
+		} else if (this.textTag) {
+			this.currentPageContents.append(text);
+		}
+	}
+
+}
--- build.sbt
+++ build.sbt
@@ -5,7 +5,10 @@ version := "1.0-SNAPSHOT"
 libraryDependencies ++= Seq(
   javaJdbc,
   javaEbean,
-  cache
+  cache,
+  "commons-io" % "commons-io" % "2.4",
+  "commons-dbutils" % "commons-dbutils" % "1.5",
+  "mysql" % "mysql-connector-java" % "5.1.30"
 )     
 
 play.Project.playJavaSettings
